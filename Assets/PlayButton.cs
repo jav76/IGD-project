71b001337b04a4fd2818be3945e275a537595886
Jaret <jav76@uakron.edu>
2022-04-19 10:59:06 -0400

    Update button

--- Assets/PlayButton.cs
+++ Assets/PlayButton.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class PlayButton : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -38,7 +39,8 @@ public class PlayButton : MonoBehaviour
 
     public void TaskOnClick()
     {
-        Application.LoadLevel(Application.loadedLevel);
-        playerTank.GetComponent<TankMovement>().Dead = false;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
     }
 }
